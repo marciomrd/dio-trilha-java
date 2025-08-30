b5a68f4a72ce38e3ed0fe6cd1608b21de72638f3
marciomrd <147844747+marciomrd@users.noreply.github.com>
2025-08-30 09:13:00 -0300

    Counter Challenge
    finishing DIO counter challenge

---
+++ DesafioControleFluxo/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+        
+        try {
+            // Chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            // Capturando a exceção personalizada e imprimindo a mensagem
+            System.out.println(e.getMessage());
+        } finally {
+            terminal.close();
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validando se parametroUm é maior que parametroDois
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        
+        // Calculando a quantidade de interações
+        int contagem = parametroDois - parametroUm;
+
+        // Realizando o loop para imprimir as mensagens
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
